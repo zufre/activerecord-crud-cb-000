0943b2b35fefaa79aa67717f4b87f961eb52004b
Cedric David <cedric.david@btopenworld.com>
2018-09-17 03:42:36 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,11 +33,11 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
       m.send("#{key}=", value)
     end
   end
-  
+
 end
 
 def can_get_the_first_item_in_the_database
-  
+  Movie.find_by(id=1)
 end
 
 def can_get_the_last_item_in_the_database
